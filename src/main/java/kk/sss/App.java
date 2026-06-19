18a3f56774b02fd5a1842a50aa334f2b6725f39a
Deeksha N <deekshadeepthi60@gmail.com>
2026-06-19 15:22:40 +0530

    Updated project files

--- src/main/java/kk/sss/App.java
+++ src/main/java/kk/sss/App.java
@@ -6,5 +6,6 @@ package kk.sss;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Worldd!");
+        System.out.println("Hello diksha!");
     }
 }
